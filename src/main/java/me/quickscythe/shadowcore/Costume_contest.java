b3eea304902d8d9f8ba85b60a2cd52938e866501
QuickScythe <QuickScythe@gmail.com>
2024-08-07 16:35:19 -0500

    init commit

---
+++ src/main/java/me/quickscythe/shadowcore/Costume_contest.java
@@ -0,0 +1,17 @@
+package me.quickscythe.shadowcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Costume_contest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
