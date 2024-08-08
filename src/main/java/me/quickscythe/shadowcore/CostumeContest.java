5c1f757e49ab552e435d5f91e0e762cee4818394
QuickScythe <QuickScythe@gmail.com>
2024-08-07 19:00:53 -0500

    jitpack and deps

--- src/main/java/me/quickscythe/shadowcore/CostumeContest.java
+++ src/main/java/me/quickscythe/shadowcore/CostumeContest.java
@@ -2,7 +2,7 @@ package me.quickscythe.shadowcore;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Costume_contest extends JavaPlugin {
+public final class CostumeContest extends JavaPlugin {
 
     @Override
     public void onEnable() {
